676e3bc1f6e81d850a4afef2571d0234fc1ba26e
JSM2512 <49087609+JSM2512@users.noreply.github.com>
2020-12-03 19:04:53 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -10,14 +10,12 @@ from email.mime.image import MIMEImage
 # import csv
 # with open(y, 'w', newline='') as file:
 #     z = csv.writer(file)
-#     z.writerow(['jaiwantmahun2000@gmail.com'])
-#     z.writerow(['jmahun_be18@thapar.edu'])
 #     z.writerow(['paEfafysav@gmail.com'])
 #     z.writerow(['SNjqsnxjx@gmail.com'])
 #     z.writerow(['ax_aa2000@gmail.com'])
 #     z.writerow(['mxaaxx2000@gmail.com'])
 
-from_add="jaiwantsingh2000@gmail.com"
+from_add="abc@gmail.com"
 to_add=[]
 with open('Email.csv','rt') as file:
     csv_rows=csv.reader(file)
@@ -35,8 +33,8 @@ msg.attach(MIMEText(body,'plain'))
 img_data=open('image.png','rb').read()
 msg.attach(MIMEImage(img_data,name=os.path.basename('image.png')))
 
-email='jaiwantsingh2000@gmail.com'
-password='jaiwant@113406'
+email='your_email_address'
+password='your_email_password'
 
 mail=smtplib.SMTP('smtp.gmail.com',587)
 mail.ehlo()
